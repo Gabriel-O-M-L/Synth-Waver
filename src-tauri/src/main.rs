1ace4f6023a42bb84bf620b0c5ce15717f1bb88f
Gabriel-O-M-L <60969331+Gabriel-O-M-L@users.noreply.github.com>
2022-10-17 11:46:42 -0300

    New commit for asynchronous calls in synthSelector

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,7 +9,6 @@ use rodio::{OutputStream, source::Source};
 fn waveTable(sampleRate: u32,capacity: usize,frequency: f32){
     let mut oscillator: synths::wavetable::WaveTableOscillator = synths::create_wave_table_oscilator(sampleRate, capacity);
     oscillator = synths::set_wave_table_frequency(oscillator, frequency);
-    print!("{:?}", oscillator);
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     let _result = stream_handle.play_raw(oscillator.convert_samples());
     std::thread::sleep(std::time::Duration::from_secs(5));
@@ -22,7 +21,7 @@ fn greet(name: &str) -> String {
 }
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![waveTable])
+        .invoke_handler(tauri::generate_handler![waveTable,greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
